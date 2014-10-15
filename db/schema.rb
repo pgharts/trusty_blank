21bc23410e2988ee7935ed6a06fadddd3845eae7
Eric Sipple <saalon@gmail.com>
2014-10-15 11:47:35 -0400

    Adding reorder extension.

--- db/schema.rb
+++ db/schema.rb
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(:version => 20120209231801) do
     t.boolean  "virtual",                               :default => false, :null => false
     t.integer  "lock_version",                          :default => 0
     t.text     "allowed_children_cache"
+    t.integer  "position"
   end
 
   add_index "pages", ["class_name"], :name => "pages_class_name"
